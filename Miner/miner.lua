1959fe529321609c1da8d4b38ff49930e1c17e19
volenko <kvolenko98@gmail.com>
2022-10-04 04:20:34 +0200

    Log system added

--- Miner/miner.lua
+++ Miner/miner.lua
@@ -8,22 +8,33 @@ local MAX_SLOTS = 14
 local BUCKET_SLOT = 15
 local CHEST_SLOT = 16
 local listOres = {"minecraft:coal_ore", "minecraft:deepslate_coal_ore",
-              "minecraft:iron_ore", "minecraft:deepslate_iron_ore",
-              "minecraft:copper_ore", "minecraft:deepslate_copper_ore",
-              "minecraft:gold_ore", "minecraft:deepslate_gold_ore",
-              "minecraft:redstone_ore", "minecraft:deepslate_redstone_ore",
-              "minecraft:emerald_ore", "minecraft:deepslate_emerald_ore",
-              "minecraft:lapis_ore", "minecraft:deepslate_lapis_ore",
-              "minecraft:diamond_ore", "minecraft:deepslate_diamond_ore",
-              "minecraft:nether_gold_ore", "minecraft:nether_quartz_ore",
-              "minecraft:ancient_debris"}
+                  "minecraft:iron_ore", "minecraft:deepslate_iron_ore",
+                  "minecraft:copper_ore", "minecraft:deepslate_copper_ore",
+                  "minecraft:gold_ore", "minecraft:deepslate_gold_ore",
+                  "minecraft:redstone_ore", "minecraft:deepslate_redstone_ore",
+                  "minecraft:emerald_ore", "minecraft:deepslate_emerald_ore",
+                  "minecraft:lapis_ore", "minecraft:deepslate_lapis_ore",
+                  "minecraft:diamond_ore", "minecraft:deepslate_diamond_ore",
+                  "minecraft:nether_gold_ore", "minecraft:nether_quartz_ore",
+                  "minecraft:ancient_debris"}
 
-if (fs.exists("ores.json")) then
-    local file = fs.open("ores.json", "r")
-    listOres = textutils.unserialiseJSON(file.readAll())
-    file.close()
-else
-    print("File \"ores.json\" is missing. I will mine default minecraft ores.")
+if (fs.exists("eldest.log")) then
+    fs.delete("eldest.log")
+end
+
+if (fs.exists("old.log")) then
+    fs.move("old.log", "eldest.log")
+end
+
+if (fs.exists("new.log")) then
+    fs.move("new.log", "old.log")
+end
+local log = fs.open("new.log", "a")
+
+
+function newLogEntry(text)
+    log.writeLine(os.day().." - "..textutils.formatTime(os.time(), true)..": "..text)
+    log.flush()
 end
 
 function drink(dir)
@@ -104,6 +115,7 @@ end
 function mine()
     local depth = 0
     while go(DOWN) do
+        newLogEntry("Level "..depth..".")
         for i=1, 4 do
             drink(FORWARD)
             if inList(turtle.inspect()) then
@@ -113,6 +125,7 @@ function mine()
         end
         depth = depth + 1
     end
+    newLogEntry("Going up.")
     for i=1, depth do
         go(UP)
     end
@@ -120,11 +133,15 @@ end
 
 
 function eat()
-    if turtle.getFuelLevel() > 5000 then return end
-    
+    if turtle.getFuelLevel() > 5000 then
+        newLogEntry("Fed up with coal.")
+        return
+    end
+
     for i=1, MAX_SLOTS do
         local detail = turtle.getItemDetail(i)
         if detail ~= nil and detail.name == "minecraft:coal" then
+            newLogEntry("Eating coal from "..i.." slot.")
             turtle.select(i)
             turtle.refuel()
         end
@@ -152,10 +169,37 @@ function nextMine()
     go(FORWARD)
 end
 
-for i=1, 10 do
-    mine()
-    eat()
-    drop()
-    nextMine()
+
+
+newLogEntry("Checking for ores.json...")
+if (fs.exists("ores.json")) then
+    local file = fs.open("ores.json", "r")
+    listOres = textutils.unserialiseJSON(file.readAll())
+    file.close()
+    newLogEntry("New list is loaded.")
+else
+    print("File \"ores.json\" is missing. I will mine default minecraft ores.")
+    newLogEntry("No list found. Default list is used.")
 end
 
+newLogEntry("Starting to mine...")
+for i=1, 10 do
+    newLogEntry(i.." cycle:")
+    if turtle.getFuelLevel() > 500 then 
+        print("Not enough fuel.\nAvailable: "..turtle.getFuelLevel().."\nRequired: 500.")
+        newLogEntry("Not enaugh fuel. Fuel level is "..turtle.getFuelLevel())
+        break
+    end
+    newLogEntry("New shaft.")
+    mine()
+    newLogEntry("Refueling.")
+    eat()
+    newLogEntry("Dropping stuff.")
+    drop()
+    newLogEntry("Moving to next mine.")
+    nextMine()
+    newLogEntry("Cycle complete.")
+end
+
+newLogEntry("Work is done.")
+log.close()
